33c5df6825408d3a79a08e357a84e04f7215a17d
Korbinian Offensperger <korbinian_offensperger@yahoo.de>
2022-05-18 23:21:01 +0200

    ~replaced arrays with lists

--- CodeWars_PowersOf2/Program.cs
+++ CodeWars_PowersOf2/Program.cs
@@ -13,48 +13,25 @@ namespace CodeWars_PowersOf2
         {
             // https://www.codewars.com/kata/57a083a57cb1f31db7000028/
 
-            BigInteger[] output = PowersOfTwo(0);
-
-            foreach (var item in output)
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.WriteLine("--------------------");
-
-            output = PowersOfTwo(1);
-
-            foreach (var item in output)
-            {
-                Console.WriteLine(item);
-            }
-
+            List<BigInteger> output = PowersOfTwo(0);
+            output.ForEach(x => Console.WriteLine(x));
             Console.WriteLine("--------------------");
 
             output = PowersOfTwo(4);
-
-            foreach (var item in output)
-            {
-                Console.WriteLine(item);
-            }
-
+            output.ForEach(x => Console.WriteLine(x));
             Console.WriteLine("--------------------");
 
-            // Hier fliegt er - Overflow vom Double. Auslagern in ne andere Methode zur Rückgabe des Werts?
             output = PowersOfTwo(100);
-
-            foreach (var item in output)
-            {
-                Console.WriteLine(item);
-            }
-
+            output.ForEach(x => Console.WriteLine(x));
             Console.WriteLine("--------------------");
         }
 
+        // ----- CAVE: Solutions differ from the actual Kata since Lists are easier to handle here! ----- //
+
         // First solution
-        public static BigInteger[] PowersOfTwo(int n)
+        public static List<BigInteger> PowersOfTwo(int n)
         {
-            BigInteger[] output = new BigInteger[n + 1];
+            List<BigInteger> output = new List<BigInteger>();
             for (int ctr = 0; ctr <= n; ctr++)
             {
                 BigInteger buffer = 1;
@@ -64,12 +41,12 @@ namespace CodeWars_PowersOf2
                     buffer *= 2;
                 }
 
-                output[ctr] = ctr == 0 ? 1 : buffer;
+                output.Add(ctr == 0 ? 1 : buffer);
             }
             return output;
         }
 
         // Refactored solution
-        public static BigInteger[] PowersOfTwoRefactored(int n) => Enumerable.Range(0, n+1).Select(x => BigInteger.Pow(2, x)).ToArray();
+        public static List<BigInteger> PowersOfTwoRefactored(int n) => Enumerable.Range(0, n+1).Select(x => BigInteger.Pow(2, x)).ToList();
     }
 }
